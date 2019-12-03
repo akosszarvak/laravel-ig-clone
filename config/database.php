8955e5d11cfaa0fce888d12a8a4fb19d55e6803d
Szarvák Ákos <43302360+re4ven@users.noreply.github.com>
2019-12-03 19:51:38 +0100

    Update db connection

--- config/database.php
+++ config/database.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Support\Str;
 
+$DATABASE_URL=parse_url('postgres://cqelygfxumfkuf:06391c25405e50e5501dd8e6a2e62c3864c1f10765e22c653a3d2990dd5119cf@ec2-174-129-253-42.compute-1.amazonaws.com:5432/d4udjbjuocc5ja');
 return [
+
 
     /*
     |--------------------------------------------------------------------------
@@ -15,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,12 +67,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+         //   'url' => env('DATABASE_URL'),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' =>$DATABASE_URL["user"],
+            'password' =>$DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
